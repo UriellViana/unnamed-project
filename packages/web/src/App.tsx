4250ab7b5cdb23160433727d88ec16f4f6c0b3fd
Bolivar <webdevelopmentbolivar@gmail.com>
2020-12-09 15:39:36 -0300

    deal with erros

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -203,6 +203,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
 import Pages from "./pages";
 import Login from "./pages/Login";
+<<<<<<< HEAD
 >>>>>>> routes scope
 import "./App.css";
 =======
@@ -309,6 +310,9 @@ const client = new ApolloClient({
 });
 =======
 import React from "react";
+=======
+import Erros from "./erros";
+>>>>>>> deal with erros
 import "./App.css";
 >>>>>>> formatting changes and some typings
 
@@ -462,13 +466,13 @@ const IS_LOGGED_IN = gql`
 function App() {
   const { data, error, loading } = useQuery(IS_LOGGED_IN);
 
-  const NOT_LOGGED_USER = "You must be logged in.";
-
-  const TOKEN = localStorage.getItem("token");
-  console.log(error);
   if (loading) return <p>Loading...</p>;
 
-  if (error && error.message === NOT_LOGGED_USER && !TOKEN) return <Login />;
+  if (error) return <Erros message={error.message} />;
+
+  const TOKEN = localStorage.getItem("token");
+
+  if (!TOKEN) return <Login />;
 
 <<<<<<< HEAD
 function App() {

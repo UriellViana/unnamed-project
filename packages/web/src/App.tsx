c4072c9af30015dfd7e5002705394f955e2a8788
Bolivar <webdevelopmentbolivar@gmail.com>
2020-12-22 14:42:33 -0300

    Types

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -543,7 +543,6 @@ export default function App() {
           path="/projects"
           Component={Projects}
           middlewares={[AuthMiddleware]}
-          Parent={null}
         />
         <TradulabRouter
           path="/"
